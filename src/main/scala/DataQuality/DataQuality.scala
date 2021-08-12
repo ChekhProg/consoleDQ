1c03bb60011bdcf2af5e6172cfc16a825bf18354
ChekhProg <antonlvovich@mail.ru>
2021-08-12 13:38:03 +0300

    delete comment

--- src/main/scala/DataQuality/DataQuality.scala
+++ src/main/scala/DataQuality/DataQuality.scala
@@ -8,16 +8,6 @@ object DataQuality {
   def getValue[T](value: Either[QualityError, T],
                   checks: List[T => Either[QualityError, T]])
                   : Either[QualityError, T] = {
-//    checks match {
-//      case Nil => Right(value)
-//      case fun :: _ => {
-//        val newValue = checks.head(value)
-//        newValue match {
-//          case Right(value) => getValue(value, checks.tail)
-//          case Left(value) => Left(value)
-//        }
-//      }
-//    }
     value match {
       case Left(exception) => Left(exception)
       case Right(value) =>
